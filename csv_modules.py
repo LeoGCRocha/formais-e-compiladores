a77ba82d134b27b42ec952aae08854ffaefa1285
lrocha <leogcrocha@gmail.com>
2022-05-18 19:54:05 -0300

    remove constantes

--- csv_modules.py
+++ csv_modules.py
@@ -1,7 +1,6 @@
 import csv
 def write_csv_file_symbol_table(filename, arr):
-    with open(filename, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(["Symbol", "Id"])
+    with open(filename,'w') as file:
         for line in arr:
-            writer.writerow(line.split('#'))
+            file.write(line)
+            file.write('\n')
--- symbol_table.py
+++ symbol_table.py
@@ -31,28 +31,26 @@ def create_symbol_table_from_file(code_file, command_reserved_keys, reserved_key
                             key_size = 0
                             if current != "":
                                 if current not in symbol_table_hash_map:
-                                    symbol_table.append("{}#id{}".format(current, last_id))
-                                    last_id+=1
-                                    symbol_table_hash_map[current] = last_id
+                                    if not current.isnumeric():
+                                        symbol_table.append("{}".format(current))
+                                        symbol_table_hash_map[current] = True
                                 current = ""
                         else:
                             current = current + line[cursor]
                 else:
                     if current != "":
                         if current not in symbol_table_hash_map:
-                            symbol_table.append("{}#id{}".format(current, last_id))
-                            last_id+=1
-                            symbol_table_hash_map[current] = last_id
+                            if not current.isnumeric():
+                                symbol_table.append("{}".format(current))
+                                symbol_table_hash_map[current] = True
                         current = ""
                 cursor = cursor + 1
                 if (cursor == len(line)):
                     if current != "":
                         if current not in symbol_table_hash_map:
-                            symbol_table.append("{}#id{}".format(current, last_id))
-                            last_id+=1
-                            symbol_table_hash_map[current] = last_id
+                            if not current.isnumeric():
+                                symbol_table.append("{}".format(current))
+                                symbol_table_hash_map[current] = True
                         current = ""
-                
-
     csv.write_csv_file_symbol_table('dummy_data/cd/generated_files/symbol_table_1.csv', symbol_table)
 create_symbol_table_from_file('dummy_data/cd/example1.txt', 'dummy_data/cd/commands_reserved_keys.txt', 'dummy_data/cd/reserved_keys.txt')
