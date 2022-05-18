5403f2e88c58eb4e6976de0894a3fcb8b9f8ca09
lrocha <leogcrocha@gmail.com>
2022-05-18 01:34:28 -0300

    work

--- symbol_table.py
+++ symbol_table.py
@@ -1,31 +1,45 @@
+from multiprocessing.dummy import current_process
+from pyparsing import line_end
 import txt_modules as txt
 import csv_modules as csv
 arr = txt.read_text_file('dummy_data/cd/example1.txt')
-def isVariable(word):
-    number_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for a in word:
-        if a not in number_list:
-            return True
-    return False
-def create_symbol_table_from_file(code_file, reserved_keys_file):
+def create_symbol_table_from_file(code_file, command_reserved_keys, reserved_keys):
     last_id = 0
     arr = txt.read_text_file(code_file)
-    arr_reserved_keys = txt.read_text_file(reserved_keys_file)
-    symbol_table_hash_map = {}
-    symble_table = []
+    reserved_command = txt.read_text_file_keys(command_reserved_keys)
+    reserved_keys = txt.read_text_file_keys(reserved_keys)
+    symbol_table = []
     for line in arr:
-        line_splited = line.split(' ')
-        for word in line_splited:
-            if word not in arr_reserved_keys:
-                if word not in symbol_table_hash_map:
-                    if isVariable(word):
-                        symbol_table_hash_map[word] = "id{}".format(last_id)
-                        symble_table.append("{}#{}".format(word, last_id))
+        # line command
+        if line in reserved_command: 
+            pass
+        else:
+            current = ""
+            cursor = 0
+            while(cursor < len(line)):
+                if line[cursor] != " ":
+                    key_size = 0 
+                    for key in reserved_keys:
+                        result = line[cursor:].find(key) + cursor
+                        if result != cursor:
+                            pass # not correct key
+                        else:
+                            if len(key) > key_size:
+                                key_size = len(key)
+                    if key_size != 0:
+                        cursor = cursor + key_size - 1
+                        key_size = 0
+                        if current != "":
+                            symbol_table.append("{}#{}".format(current, last_id))
+                            last_id+=1
+                            current = ""
+                    else:
+                        current = current + line[cursor]
+                cursor = cursor + 1
+                if (cursor == len(line)):
+                    if current != "":
+                        symbol_table.append("{}#{}".format(current, last_id))
                         last_id+=1
-                else:
-                    if isVariable(word):
-                        symble_table.append("{}#{}".format(word, last_id))
-                        last_id+=1
-    # save on .csv
-    csv.write_csv_file_symbol_table('dummy_data/cd/generated_files/symbol_table_1.csv', symble_table)
-create_symbol_table_from_file('dummy_data/cd/example1.txt', 'dummy_data/cd/example1_reserved_keys.txt')
+                        current = ""
+    print(symbol_table)
+create_symbol_table_from_file('dummy_data/cd/example1.txt', 'dummy_data/cd/commands_reserved_keys.txt', 'dummy_data/cd/reserved_keys.txt')
