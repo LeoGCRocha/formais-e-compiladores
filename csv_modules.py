bf485ad2d2a64b4f60e9e3151a74a823851f003e
lrocha <leogcrocha@gmail.com>
2022-05-18 01:42:15 -0300

    Finish

--- csv_modules.py
+++ csv_modules.py
@@ -1,6 +1,6 @@
 import csv
 def write_csv_file_symbol_table(filename, arr):
-    with open(filename, 'w', newline='') as file:
+    with open(filename, 'w') as file:
         writer = csv.writer(file)
         writer.writerow(["Symbol", "Id"])
         for line in arr:
--- symbol_table.py
+++ symbol_table.py
@@ -1,5 +1,3 @@
-from multiprocessing.dummy import current_process
-from pyparsing import line_end
 import txt_modules as txt
 import csv_modules as csv
 arr = txt.read_text_file('dummy_data/cd/example1.txt')
@@ -9,6 +7,7 @@ def create_symbol_table_from_file(code_file, command_reserved_keys, reserved_key
     reserved_command = txt.read_text_file_keys(command_reserved_keys)
     reserved_keys = txt.read_text_file_keys(reserved_keys)
     symbol_table = []
+    symbol_table_hash_map = {}
     for line in arr:
         # line command
         if line in reserved_command: 
@@ -30,16 +29,20 @@ def create_symbol_table_from_file(code_file, command_reserved_keys, reserved_key
                         cursor = cursor + key_size - 1
                         key_size = 0
                         if current != "":
-                            symbol_table.append("{}#{}".format(current, last_id))
-                            last_id+=1
+                            if current not in symbol_table_hash_map:
+                                symbol_table.append("{}#id{}".format(current, last_id))
+                                last_id+=1
+                                symbol_table_hash_map[current] = last_id
                             current = ""
                     else:
                         current = current + line[cursor]
                 cursor = cursor + 1
                 if (cursor == len(line)):
                     if current != "":
-                        symbol_table.append("{}#{}".format(current, last_id))
-                        last_id+=1
+                        if current not in symbol_table_hash_map:
+                            symbol_table.append("{}#id{}".format(current, last_id))
+                            last_id+=1
+                            symbol_table_hash_map[current] = last_id
                         current = ""
-    print(symbol_table)
+    csv.write_csv_file_symbol_table('dummy_data/cd/generated_files/symbol_table_1.csv', symbol_table)
 create_symbol_table_from_file('dummy_data/cd/example1.txt', 'dummy_data/cd/commands_reserved_keys.txt', 'dummy_data/cd/reserved_keys.txt')
