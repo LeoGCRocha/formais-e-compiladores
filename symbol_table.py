5f40491c6e8dbc430c86a86a90c06968e2ad3910
lrocha <leogcrocha@gmail.com>
2022-05-18 21:59:15 -0300

    json file config

--- symbol_table.py
+++ symbol_table.py
@@ -1,17 +1,22 @@
-from itertools import count
+import re
 import txt_modules as txt
 import csv_modules as csv
-arr = txt.read_text_file('dummy_data/cd/example1.txt')
-def create_symbol_table_from_file(code_file, command_reserved_keys, reserved_keys):
+import json 
+def adjustReservedWord(reserved_word_list):
+    for i in range(0, len(reserved_word_list)):
+        reserved_word_list[i] = reserved_word_list[i] + " "
+    return reserved_word_list
+def create_symbol_table_from_file(code_file):
     last_id = 0
     arr = txt.read_text_file(code_file)
-    reserved_command = txt.read_text_file_keys(command_reserved_keys)
-    reserved_keys = txt.read_text_file_keys(reserved_keys)
+    arr = txt.read_text_file('dummy_data/cd/example1.txt')
+    Symbol_Table = json.load(open("config.json"))
+    reserved = adjustReservedWord(Symbol_Table['reserved']) + Symbol_Table['assignment'] + Symbol_Table['operators']
     symbol_table = []
     symbol_table_hash_map = {}
     for line in arr:
         # line command
-        if line in reserved_command: 
+        if line in reserved:
             pass
         else:
             current = ""
@@ -19,7 +24,7 @@ def create_symbol_table_from_file(code_file, command_reserved_keys, reserved_key
             while(cursor < len(line)):
                 if line[cursor] != " ":
                         key_size = 0 
-                        for key in reserved_keys:
+                        for key in reserved:
                             result = line[cursor:].find(key) + cursor
                             if result != cursor:
                                 pass # not correct key
@@ -53,4 +58,4 @@ def create_symbol_table_from_file(code_file, command_reserved_keys, reserved_key
                                 symbol_table_hash_map[current] = True
                         current = ""
     csv.write_csv_file_symbol_table('dummy_data/cd/generated_files/symbol_table_1.csv', symbol_table)
-create_symbol_table_from_file('dummy_data/cd/example1.txt', 'dummy_data/cd/commands_reserved_keys.txt', 'dummy_data/cd/reserved_keys.txt')
+create_symbol_table_from_file('dummy_data/cd/example1.txt')
