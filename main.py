8b7ddebc42c4a2e831b97f070ff34f14cee0e9ad
Nicolas Vanz <nicolas.vanz@outlook.com>
2022-06-12 00:56:21 -0300

    updated main

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from files import *
 
 
 if __name__ == "__main__":
-    
     files = glob.glob("outputs/csv_files/*.csv")
     for f in files:
         os.remove(f)
@@ -36,3 +35,10 @@ if __name__ == "__main__":
     automata = t4()
     automata_to_csv(Files.CSV_4, automata, automata.listOfSymbols())
     csv_to_table(Files.CSV_4, Files.TABLE_4)
+
+    lexical_analyzer = LexicalAnalyser("inputs/language.txt")
+    automata_to_csv("outputs/csv_files/AutomatoLanguages.csv", lexical_analyzer.DFA, lexical_analyzer.DFA.listOfSymbols())
+    csv_to_table("outputs/csv_files/AutomatoLanguages.csv", "outputs/table_result/AutomatoLanguages.csv")
+    tokens = lexical_analyzer.tokenize()
+    tokens_to_txt("outputs/tokens.txt", tokens)
+    symbol_table_to_csv("outputs/symboltable.csv", lexical_analyzer.symbolTable.symbols)
