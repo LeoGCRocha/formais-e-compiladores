73cfb90197a40233d2a719f1e7f15caebeac527e
lrocha <leogcrocha@gmail.com>
2022-07-02 22:22:37 -0300

    Fix recursion problem

--- left_recursion.py
+++ left_recursion.py
@@ -29,21 +29,24 @@ def eliminateIndirectRecursion(productions):
         # Remove indirect recursion
         for keyDic, valueDic in productions.items():
             if j > i:
+                values = []
                 # Iterate in all sentences
                 for sentence in valueDic:
                     # If the first symbol is the same as the key, then replace the first symbol
                     if sentence[0] == key:
                         # Replace if all possibilites
-                        values = []
-                        for valueToAdd in value:
-                            values.append(valueToAdd + sentence[1:])
-                        productions[keyDic] = values
+                        # TODO: Fix this
+                        pass
+                    # else:
+                    #     values.append(sentence)
+                productions[keyDic] = values
             j = j + 1
         j = 0
         i = i + 1
     return productions
 withoutIndirect = eliminateIndirectRecursion(withoutEpsilon)
 print(withoutIndirect)
+# Eliminate Direct Recursion
 def eliminateDirectRecursion(productions):
     dic_without_recursion = {}
     for key, value in productions.items():
@@ -56,7 +59,31 @@ def eliminateDirectRecursion(productions):
             dic_without_recursion[key] = value
         else:
             # Is left recursive, remove the first symbol
-            pass
+            newKeyValue = key + "'"
+            resursive_values = []
+            not_recursive_values = []
+            for sentence in value:
+                if sentence[0] == key:
+                    # A -> Aa 
+                    resursive_values.append(sentence[1:] + newKeyValue)
+                else:
+                    # E -> ab | EC
+                    # E -> abE'
+                    not_recursive_values.append(sentence + newKeyValue)
+            resursive_values.append("&")
+            dic_without_recursion[key] = not_recursive_values
+            dic_without_recursion[newKeyValue] = resursive_values
     return dic_without_recursion
+# Output dictionary
+def dicToFile(dic, file):
+    f = open(file, 'w')
+    for key, value in dic.items():
+        f.write(key + " -> ")
+        stringToWrite = ""
+        for sentence in value:
+            stringToWrite = stringToWrite + sentence + " | "
+        stringToWrite = stringToWrite[:-3]
+        f.write(stringToWrite + "\n")
+    f.close()
 withoutDirect = eliminateDirectRecursion(withoutIndirect)
-print(withoutDirect)
+dicToFile(withoutDirect, "outputs/left.txt")
