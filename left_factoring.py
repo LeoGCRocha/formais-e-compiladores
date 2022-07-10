a87a704b309b26c5942f433f00e66b38995a3d49
nicolasvanz <nicolas.vanz@outlook.com>
2022-07-10 19:55:14 -0300

    enhancement: recursion at left_recursion & code improvements

--- left_factoring.py
+++ left_factoring.py
@@ -1,54 +1,67 @@
-def LeftFactoring(productions):
-    # Example   
+from operators import Operators as OP
+
+def leftFactoring(language):
+    for key, value in language.items():
+        language[key] = list(map(lambda x: x if x != "" else OP.EPSILON, value))
+
+    # Example
     # S-> ad|ac|b
-    newDict = {}
+    newLanguage = {}
     # Ireterate in all productions
-    for key in productions:
-        prods = productions[key]
-        # prods : [ad, ac, b]
-        temp = dict()
+    for key, productions in language.items():
+        # productions = language[key]
+        # productions : [ad, ac, b]
+        subproductions = dict()
         # Array with the transitions to each symbol
         # a -> [ad, ac] && b -> [b]
-        for subprod in prods:
-            if subprod[0] not in list(temp.keys()):
-                temp[subprod[0]] = [subprod]
+        for subproduction in productions:
+            if subproduction[0] not in list(subproductions.keys()):
+                subproductions[subproduction[0]] = [subproduction]
             else:
-                temp[subprod[0]].append(subprod)
-        print(temp)
-        # if value list count for any key in temp is > 1,
+                subproductions[subproduction[0]].append(subproduction)
+        # if value list count for any key in subproductions is > 1,
         # - it has left factoring
         # new_rule stores new subrules for current LHS symbol
-        new_rule = []
+        newRule = []
         # temp_dict stores new subrules for left factoring
-        tempo_dict = {}
-        for term_key in temp:
+        newSubrules = {}
+        for term_key, allStartingWithTermKey in subproductions.items():
             # get value from temp for term_key
-            allStartingWithTermKey = temp[term_key]
+            # allStartingWithTermKey = subproductions[term_key]
             if len(allStartingWithTermKey) > 1:
                 # left factoring required
                 # to generate new unique symbol
                 # - add ' till unique not generated
                 lhs_ = key + "'"
-                while (lhs_ in productions.keys()) \
-                        or (lhs_ in tempo_dict.keys()):
+                while (lhs_ in language.keys()) \
+                    or (lhs_ in newSubrules.keys()):
                     lhs_ += "'"
                 # append the left factored result
-                new_rule.append([term_key, lhs_])
-                # add expanded rules to tempo_dict
+                newRule.append([term_key, lhs_])
+                # add expanded rules to newSubrules
                 ex_rules = []
-                for g in temp[term_key]:
+                for g in subproductions[term_key]:
                     ex_rules.append(g[1:])
-                tempo_dict[lhs_] = ex_rules
+                newSubrules[lhs_] = ex_rules
             else:
                 # no left factoring required
-                new_rule.append(allStartingWithTermKey[0])
+                newRule.append(allStartingWithTermKey[0])
         # add original rule
-        newDict[key] = new_rule
+        newLanguage[key] = newRule
         # add newly generated rules after left factoring
-        for key in tempo_dict:
-            newDict[key] = tempo_dict[key]
-    return newDict
-rules= {"S": ["ad", "ac", "b"]}
-result= LeftFactoring(rules)
+        for key in newSubrules:
+            newLanguage[key] = newSubrules[key]
+
+    if (language != newLanguage):
+        return leftFactoring(newLanguage)
+
+    return newLanguage
+
+rules= {
+    "S": ["acd", "ac"], 
+    "A": ["bd", "bdef"],
+}
+# rules= {"S": ["c", "a", "b"]}
+result= leftFactoring(rules)
 for y in result:    
     print(y, "->", result[y])
